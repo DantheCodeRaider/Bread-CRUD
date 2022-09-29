1684ffd4d2ce1b72c7300f89f5eb2024462cacbf
Dan <dan@coderaider.tech>
2022-09-29 18:45:22 -0500

    Code A3 complete

---
+++ views/fourofour.jsx
@@ -0,0 +1,14 @@
+const React = require('react')
+const Default = require('./layouts/Default')
+
+function fourofour () {
+    // Confirm we are getting our bread data in the terminal.
+    // console.log(bread.name)
+      return (
+        <Default>
+        <h2><a href="/">404 Page</a></h2>
+        </Default>
+      )
+  }
+  
+module.exports = fourofour
